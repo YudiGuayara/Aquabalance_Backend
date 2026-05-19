eaf2837966472656f991deb9b17f1898b361f409
Yudi Cruz <karinaguayara592@gmail.com>
2026-05-19 01:49:17 -0500

    implementación módulos de eventos y alertas.

--- src/main/java/com/AquaBalance/Events/infrastructure/persistence/JpaContaminanteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.AquaBalance.monitoreo.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaContaminanteRepository extends JpaRepository<ContaminanteEntity, Long> {
-}
